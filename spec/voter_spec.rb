4d27632922a45bbffaf1d0bc842a28e60b091a26
compwron <wordymusic@gmail.com>
2013-06-09 15:51:46 -0500

    WIP nonrunning tests on assigning a vote on a topic to another user, and retrieving it

--- spec/voter_spec.rb
+++ spec/voter_spec.rb
@@ -7,4 +7,23 @@ describe Voter do
     it "should have a vote" do
     	subject.vote.should_not be_nil
     end
+
+    it "should be able to assign their vote for a specific topic to another voter" do
+    	other_voter = Voter.new
+    	subject.flow_vote_on_topic_to(Topics.Healthcare, other_voter) # to flow here means to assign a vote to another voter
+    	other_voter.all_votes.should_include(Vote.new(Topics.Healthcare, subject))
+    	subject.vote_on_topic_belongs_to(Topics.Healthcare, other_voter)
+    end
+
+    it "should be able to revoke vote flowed to another voter" do
+    	healthcare_vote = (Vote.new(Topics.Healthcare, subject))
+    	
+    	other_voter = Voter.new
+    	subject.flow_vote_on_topic_to(Topics.Healthcare, other_voter) # to flow here means to assign a vote to another voter
+    	other_voter.all_votes.should include healthcare_vote
+    	
+    	subject.retrieve_vote(Topics.Healthcare)
+    	other_voter.all_votes.should_not include healthcare_vote
+    end
+
 end
