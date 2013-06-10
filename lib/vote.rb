84ff3854396fd7684c731e9f7a2c7be64a26406d
compwron <wordymusic@gmail.com>
2013-06-09 19:41:23 -0500

    WIP working on vote flow

--- lib/vote.rb
+++ lib/vote.rb
@@ -1,6 +1,6 @@
 class Vote
 	attr_reader :assigned_to
-	def initialize(purpose)
+	def initialize(purpose, original_vote_owner=self)
 		@assigned_to = purpose
 	end
 end
--- lib/voter.rb
+++ lib/voter.rb
@@ -4,4 +4,8 @@ class Voter
 	def initialize()
 		@vote = Vote.new(Topics.Healthcare) # how will this work?
 	end
+
+	def flow_vote_on_topic_to(purpose, other_voter)
+		@flow_votes += new Vote(purpose, other_voter)
+	end
 end
